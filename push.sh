e3c28243a5f794d05dc6ff8b941119fedf5607c7
monlor <monlor@qq.com>
2018-07-26 18:15:00 +0800

    2018-07-26 18:15:00

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "init"
+git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git remote rm origin
 git remote add origin git@github.com:monlor/monlor.github.io.git
 git push origin hexo -f
